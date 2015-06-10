3640e5c79c40399bb13ed8b7b77112d83844f368
Tim Vergenz <vergenzt@gmail.com>
2015-06-10 16:21:35 -0400

    Add basic endpoints

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 var express = require('express');
 var app = express();
 
+// service dependencies
 var github = require('./services/github');
 var rtm = require('./services/rtm');
+
+// configure middleware
+app.use(require('body-parser').json());
+
+// github endpoint
+app.post('/github_changed', function(req, res) {
+  console.log(req.body);
+});
+
+// rtm endpoint
+app.post('/rtm_changed', function(req, res) {
+  console.log(req.body);
+});
 
